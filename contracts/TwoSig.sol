98ce4cef4fb278575538323c387382f325d1ca2e
Olivier Roques <olivier.roques@telecom-paristech.fr>
2019-01-17 10:51:09 +0000

    Add comments to code

--- contracts/TwoSig.sol
+++ contracts/TwoSig.sol
@@ -2,39 +2,51 @@ pragma solidity ^0.4.24;
 
 contract TwoSig {
 
+    // 'recipent' will receive the payment
+    address private recipient;
+    // Declare the addresses of the two participants 'partyA' and 'partyB'
     address private partyA;
     address private partyB;
-    address private recipient;
+    // Declare the mapping 'registered'
     mapping(address => bool) private registered;
 
     constructor(address addrA, address addrB, address addrRecipient) public {
+        recipient = addrRecipient;
+        // Initialize the addresses of participants
         partyA    = addrA;
         partyB    = addrB;
-        recipient = addrRecipient;
 
+        // Initialize the mapping for participants 'partyA' and 'partyB'
         registered[partyA] = false;
         registered[partyB] = false;
     }
 
     modifier isParticipant() {
+        // This modifier tests if a message is from one of the two participants
         require (msg.sender == partyA || msg.sender == partyB);
         _;
     }
 
     modifier isNotRegistered() {
+        // This modifier tests if the participant has already registered
         require (registered[msg.sender] == false);
         _;
     }
 
+    // Use the two modifiers above to create public function 'register'
     function register() public isParticipant() isNotRegistered() {
         registered[msg.sender] = true;
     }
 
     modifier allRegistered() {
+        // This modifier tests if both participants have registered
         require (registered[partyA] == true && registered[partyB] == true);
         _;
     }
 
+    // Use the modifier above to create public function 'action'
+    // This function should transfer the contract's balance to 'recipeint'
+    // Don't forget to reset the mapping !
     function action() public allRegistered() {
         recipient.transfer(address(this).balance);
         registered[partyA] = false;
