1648eff18cf764a8670270f0a06704d1438e826e
Olivier Roques <olivier.roques@telecom-paristech.fr>
2019-01-17 10:55:48 +0000

    Update comments to code

--- contracts/TwoSig.sol
+++ contracts/TwoSig.sol
@@ -7,7 +7,7 @@ contract TwoSig {
     // Declare the addresses of the two participants 'partyA' and 'partyB'
     address private partyA;
     address private partyB;
-    // Declare the mapping 'registered'
+    // Declare the mapping 'registered' which maps an address to a boolean
     mapping(address => bool) private registered;
 
     constructor(address addrA, address addrB, address addrRecipient) public {
@@ -22,31 +22,32 @@ contract TwoSig {
     }
 
     modifier isParticipant() {
-        // This modifier tests if a message is from one of the two participants
+        // This modifier checks if a message is from one of the two participants
         require (msg.sender == partyA || msg.sender == partyB);
         _;
     }
 
     modifier isNotRegistered() {
-        // This modifier tests if the participant has already registered
+        // This modifier checks if a participant has already registered
         require (registered[msg.sender] == false);
         _;
     }
 
     // Use the two modifiers above to create public function 'register'
+    // This function registers a participant by updating the mapping
     function register() public isParticipant() isNotRegistered() {
         registered[msg.sender] = true;
     }
 
     modifier allRegistered() {
-        // This modifier tests if both participants have registered
+        // This modifier checks if both participants have registered
         require (registered[partyA] == true && registered[partyB] == true);
         _;
     }
 
     // Use the modifier above to create public function 'action'
     // This function should transfer the contract's balance to 'recipeint'
-    // Don't forget to reset the mapping !
+    // Don't forget to reset the mapping afterwards !
     function action() public allRegistered() {
         recipient.transfer(address(this).balance);
         registered[partyA] = false;
